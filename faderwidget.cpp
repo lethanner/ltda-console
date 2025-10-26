2bfdaff821dae8a9d1efd66fdc37669db28ff7c9
lethanner <50079644+lethanner@users.noreply.github.com>
2025-10-26 23:03:17 +0400

    remove unused file

--- faderwidget.cpp
+++
@@ -1,9 +0,0 @@
-// #include "mixerchannel.h"
-
-// FaderWidget::FaderWidget(QWidget *parent)
-//     : QWidget{ parent } {
-
-
-//     setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
-//     setStyleSheet("background:#222");
-// }
